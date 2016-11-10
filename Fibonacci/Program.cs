bbf4485c673656e552d51467c18f423dc74dff8d
Maxwell Santos <maxwell.santos@iterative.com.br>
2016-11-10 17:17:28 -0200

    Fibonacci - exercise

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double anterior = 1;
+            double proximo = 1;
+            double auxiliar = 0;
+            string saida = anterior + ", " + proximo + ", ";
+            for (int i = 1; i <= 100; i++)
+            {
+                auxiliar = proximo;
+                proximo += anterior;
+                anterior = auxiliar;
+                saida += proximo + ", ";
+                
+            }
+
+            Console.WriteLine(saida);
+        }
+    }
+}
